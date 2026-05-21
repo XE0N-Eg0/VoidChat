0034b6a9fea50bca9f630292a5ae2c2792f4be8a
XE0N_eg0 <work.raghu101@gmail.com>
2026-05-21 14:28:20 +0530

    Added a basic db module to work

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from crypto.encrypt import test
 from protocol.sender import main
-
+from db.db import test as test2
 
 if __name__ == "__main__":
     print(__name__)
     test()
     main()
+    test2()
